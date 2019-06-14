253315d202340f513db2d77b72cabd3bb1e4d7a8
Cristian Granda <cristian.granda.pastor@gmail.com>
2019-06-14 09:41:39 -0500

    Add more css

--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,5 +1,6 @@
 /** @jsx jsx */
 import React from "react";
+import "@reach/dialog/styles.css";
 import VisuallyHidden from "@reach/visually-hidden";
 import { Dialog, DialogOverlay, DialogContent } from "@reach/dialog";
 import { jsx } from "@emotion/core";
@@ -17,11 +18,13 @@ function CommentForm({ showDialog, handleDialogOff }) {
     borderRadius: "50%",
     width: "50px",
     height: "50px",
-    fontSize: "2em",
+    fontSize: "calc(1.4em + 1vw)",
     alignSelf: "flex-end"
   };
 
-  const styledDialogOverlay = { background: "rgba(255, 255, 255, 0.9)" };
+  const styledDialogOverlay = {
+    background: "rgba(255, 255, 255, 0.9)"
+  };
 
   const styledDialogContent = {
     boxShadow: "rgba(0, 0, 0, 0.34) 0px 10px 50px",
@@ -34,6 +37,26 @@ function CommentForm({ showDialog, handleDialogOff }) {
   const styledForm = {
     display: "flex",
     flexDirection: "column"
+  };
+
+  const styledLabel = {
+    fontSize: "calc(1em + 1vw)"
+  };
+
+  const styledTextArea = {
+    height: "30vh"
+  };
+
+  const styledSubmit = {
+    backgroundColor: "#8eff47",
+    border: "none",
+    cursor: "pointer",
+    borderRadius: "15px",
+    width: "45%",
+    height: "50px",
+    fontSize: "calc(1em + 1vw)",
+    alignSelf: "center",
+    margin: "10px 0"
   };
 
   return (
@@ -52,14 +75,19 @@ function CommentForm({ showDialog, handleDialogOff }) {
           <span aria-hidden>×</span>
         </button>
         <form onSubmit={handleSubmit} css={styledForm}>
-          <label htmlFor="body-comment">Comment</label>
+          <label htmlFor="body-comment" css={styledLabel}>
+            Comment
+          </label>
           <textarea
             autoFocus
             type="text"
             name="body-comment"
             id="body-comment"
+            css={styledTextArea}
           />
-          <button type="submit">Create</button>
+          <button type="submit" css={styledSubmit}>
+            Create
+          </button>
         </form>
       </DialogContent>
     </DialogOverlay>
